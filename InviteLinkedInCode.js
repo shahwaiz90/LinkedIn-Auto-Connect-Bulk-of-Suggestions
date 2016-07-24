637586cd2e68c7d084fb2cecde550dca9649aaf3
Ahmad Shahwaiz <shahwaiz90@yahoo.com>
2016-07-24 09:03:59 +0500

    Create InviteLinkedInCode.js

---
+++ InviteLinkedInCode.js
@@ -0,0 +1,14 @@
+//The code was orignally created by Tanvir Zafar (https://www.facebook.com/tanvirzafar123). I just refactored, commented and fixed some errors.
+//Copy & Paste this code in the Console After opening (https://www.linkedin.com/people/pymk?)
+//It will run after every 3 second.
+setInterval(function() {
+    //Get the Connect Buttons available in the "People You May Know" Page i.e: 12
+    var connectButtons = document.getElementsByClassName('bt-request-buffed');
+    for (var i = 0; i < connectButtons.length; i++) { //Iterate to 12 Invitations 
+        //Returning the Name of the Person
+        console.log(connectBtns[i].parentNode.children[0].children[1].children[0].textContent);
+        //Sending the Invitation
+        connectBtns[i].click();
+    }
+}, 3000);
+
